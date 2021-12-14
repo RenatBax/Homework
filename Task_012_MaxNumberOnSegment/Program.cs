5a4cc6299eb5af720ba452ccc830c9243a699f2a
RenatBax <renchik_125.111@mail.ru>
2021-12-14 23:23:13 +0300

    Задача 12 тупик

---
+++ Task_012_MaxNumberOnSegment/Program.cs
@@ -0,0 +1,30 @@
+﻿// 12. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int Namber = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число на отрезке [10, 99] = {Namber}.");
+Console.WriteLine();
+// int[] CreateArrayNamber(int Nambers)
+// {
+//     int[] NambersMax = new int[2];
+//     for(int index = 0; index < 1; index++)
+//     NambersMax[index] = Nambers;
+//     return NambersMax;
+// }
+// void PrintArray(int[] NamberMax)
+// {
+//     for(int index = 0; index < 1; index++)
+//     Console.Write(NamberMax[1] +" ");
+// }
+// int[] N = CreateArrayNamber(Nambers);
+// PrintArray(N);
+
+int[] arr = new int[Namber];
+for(int i = 0; i < 1; i++)
+{
+    Console.Write(arr[i] + " ");
+    for(int j = 0; j < 1; j++)
+    {
+    Console.Write(arr[j] + " ");
+    }
+}
+//int[] a = new int[arr];

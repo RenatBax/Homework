def27b8c4fd45dc31bd25de6abfeef4b025d2df7
RenatBax <renchik_125.111@mail.ru>
2021-12-14 23:33:35 +0300

    Задача 13 тупик

---
+++ Task_013_DeleteSecondNumber/Program.cs
@@ -0,0 +1,10 @@
+﻿// 13. Удалить вторую цифру трёхзначного числа
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a - (a%10);
+int result = (b/10)%10;
+Console.WriteLine($"Вторая цифра трехзначного числа равна: {result}.");
+Console.Write(value: $"Давайте удалим вторую цифру? Нажмите любую клавишу...");
+Console.ReadKey();
+//int[] SecondNanber = new int[b];

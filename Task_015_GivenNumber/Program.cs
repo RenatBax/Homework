b5b63d4f1d183468b16060283aa6bed7055c82ef
RenatBax <renchik_125.111@mail.ru>
2021-12-14 23:49:04 +0300

    Сделана задача 15

---
+++ Task_015_GivenNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿// 15. Дано число. Проверить кратно ли оно 7 и 23
+
+// Console.WriteLine("Введите число: "); //Проверка, все работает
+// int Namber = Convert.ToInt32(Console.ReadLine());
+
+for(int Namber = 1; Namber < 1000; Namber++) // Доработка с семинара, подсказал Андрей Заярный
+{
+
+// int Namber = new Random().Next(-1000, 1000);
+// Console.WriteLine($" Дано число: {Namber}.");
+//int count; // Доделать автомат введение, до нахождения
+if(Namber % 7 == 0 && Namber % 23 == 0)  //if
+{
+    
+    Console.WriteLine($"Случайное число {Namber}, кратно 7 и 23");
+    break; // Остановка если нашли
+}
+    else
+    Console.WriteLine($"Случайное число {Namber} нам не подходит");
+}

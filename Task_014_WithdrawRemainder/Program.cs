79135575571678f4cf1e25c27d03fc57cf56f98e
RenatBax <renchik_125.111@mail.ru>
2021-12-14 23:37:16 +0300

    Сделана задача 14

---
+++ Task_014_WithdrawRemainder/Program.cs
@@ -0,0 +1,20 @@
+﻿// 14. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.WriteLine("Введите число: ");
+int Namber = Convert.ToInt32(Console.ReadLine());
+
+int Nambers = new Random().Next(-1000, 1000);
+Console.WriteLine($"Случайное заданное число = {Nambers}.");
+decimal result = (decimal)Nambers /  (decimal)Namber;
+Console.WriteLine($"В результате деления получилочь число: {result}.");
+Console.ReadKey();
+if(result % 2 == 0)
+{
+    Console.WriteLine($"Введенное число кратно заданному и равно: {result}.");
+}
+else
+
+{
+    decimal Remains = result - (result - (result % 2));
+    Console.WriteLine($"Число некратное, остаток равен: {Remains}.");
+}

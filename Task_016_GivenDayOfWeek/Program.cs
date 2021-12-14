de4328e6e6678c32a35cb9b8b5b24d0f9d5c516b
RenatBax <renchik_125.111@mail.ru>
2021-12-15 00:18:18 +0300

    Сделана задача 16

---
+++ Task_016_GivenDayOfWeek/Program.cs
@@ -0,0 +1,12 @@
+﻿// 16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+int rnd = new Random().Next(1, 8);
+Console.WriteLine("Дано число, обозначающее день недели: " +rnd );
+
+void givenDayOfWeek(int a)
+{
+    if(a <= 5) Console.WriteLine("Вам не повезло, рабочий день("); 
+    if(a == 6) Console.WriteLine("Ура! Выходной день, суббота!");
+    if(a == 7) Console.WriteLine("Ура! Выходной день, воскрессенье!");
+}
+givenDayOfWeek(rnd);

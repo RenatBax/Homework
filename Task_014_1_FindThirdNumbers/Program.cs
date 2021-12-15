e8a9c20dc0e35a773b1aaf0b7dc22c4c872ef204
RenatBax <renchik_125.111@mail.ru>
2021-12-15 23:52:31 +0300

    Сделана задача 14_1

---
+++ Task_014_1_FindThirdNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿// 15. Найти третью цифру числа или сообщить, что её нет
+
+void FindThirdNumber(string text)
+{
+    int length = text.Length;
+    int i = 0;
+    if (length < 3) Console.WriteLine("Третьего числа нет");
+    else while (i < length)
+    {
+        if (i >= 2)
+        {
+            Console.WriteLine($"Третье число = {text[2]}");
+            break;
+        }
+        i++;
+    }
+}
+
+Console.Write("Введите число: ");
+string S = Console.ReadLine();
+FindThirdNumber(S);

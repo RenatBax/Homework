b29b1047dd1486d1ec94c31d403caf54db70197b
RenatBax <renchik_125.111@mail.ru>
2021-12-16 00:10:06 +0300

    Сделана задача 13

--- Task_013_DeleteSecondNumber/Program.cs
+++ Task_013_DeleteSecondNumber/Program.cs
@@ -1,10 +1,22 @@
 ﻿// 13. Удалить вторую цифру трёхзначного числа
 
-Console.Write("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-int b = a - (a%10);
-int result = (b/10)%10;
-Console.WriteLine($"Вторая цифра трехзначного числа равна: {result}.");
-Console.Write(value: $"Давайте удалим вторую цифру? Нажмите любую клавишу...");
-Console.ReadKey();
+void FindThirdNumber(string text)
+{
+    Console.WriteLine($"Вторая цифра {text[0]}{ text[2]}");
+}
+
+Console.Write("Введите трехзначное число: ");
+string S = Console.ReadLine();
+FindThirdNumber(S);
+
+
+// Второй вариант решения - не доделанный
+
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = a - (a%10);
+// int result = (b/10)%10;
+// Console.WriteLine($"Вторая цифра трехзначного числа равна: {result}.");
+// Console.Write(value: $"Давайте удалим вторую цифру? Нажмите любую клавишу...");
+// Console.ReadKey();
 //int[] SecondNanber = new int[b];

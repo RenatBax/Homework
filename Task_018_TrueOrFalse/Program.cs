b82a9c420e1a98c6387817e0078d1ea26815cbeb
RenatBax <renchik_125.111@mail.ru>
2021-12-17 22:31:52 +0300

    Сделана задача 18

---
+++ Task_018_TrueOrFalse/Program.cs
@@ -0,0 +1,18 @@
+﻿// 18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+void trueOrFalse(bool x, bool y)
+{
+    if (!(x ^ y) == !x & !y) Console.WriteLine("Утверждение верно");
+    else
+    {
+        Console.WriteLine("Утверждение не верно");
+    }
+}
+
+bool x = true;
+bool y = false;
+trueOrFalse(x, y);
+
+
+
+ 

4cd97987de610d960a28b074c519db32fdbd90cd
RenatBax <renchik_125.111@mail.ru>
2021-12-20 01:08:07 +0300

    Сделана задача 23

---
+++ Task_023_TableOfSquares/Program.cs
@@ -0,0 +1,18 @@
+﻿// 23. Показать таблицу квадратов чисел от 1 до N
+
+void tableOfSquares(int a)
+{
+    int maxN = a;
+    int minN = 1;
+    Console.WriteLine($"а | а х а"); 
+    while(minN <= maxN)
+    {
+        int square = minN * minN;
+        Console.WriteLine($"{minN} |   {square}");  
+        minN++;
+    }  
+}
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+tableOfSquares(a);

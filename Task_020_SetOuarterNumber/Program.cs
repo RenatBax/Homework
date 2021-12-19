fd1d81e436c44229930e3624730f9fa9f1fbdb31
RenatBax <renchik_125.111@mail.ru>
2021-12-19 22:34:37 +0300

    Сделана задача 20

---
+++ Task_020_SetOuarterNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿// 20. Задать номер четверти, 
+//показать диапазоны для возможных координат
+
+void setQuarterNumber(int a)
+{
+    int max = a;
+    int min = -a;    
+    if ( a == 1) Console.WriteLine($"Диапазон возможных координат по Х и У от 0 до +{max} и больше.");
+    if ( a == 2) Console.WriteLine($"Диапазон возможных координат по Х от 0 до {min} и меньше, по У от 0 до +{max} и больше.");
+    if ( a == 3) Console.WriteLine($"Диапазон возможных координат по Х и У от 0 до {min} и меньше.");
+    if ( a == 4) Console.WriteLine($"Диапазон возможных координат по Х от 0 до +{max} и больше, по У от 0 до {min} и меньше.");
+}
+Console.Write("Введите номер четверти (их всего четыре): ");
+int numberQuarter = Convert.ToInt32(Console.ReadLine());
+setQuarterNumber(numberQuarter);

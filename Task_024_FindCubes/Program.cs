ae5e4d968f551c95e4904c1646f86351ae001a62
RenatBax <renchik_125.111@mail.ru>
2021-12-20 01:25:27 +0300

    Сделана задача 24

---
+++ Task_024_FindCubes/Program.cs
@@ -0,0 +1,20 @@
+﻿// 24. Найти кубы чисел от 1 до N
+
+void findCubes(double a)
+{
+    double maxN = a;
+    double minN = 1;
+    Console.WriteLine($"а | а в кубе"); 
+    while(minN <= maxN)
+    {
+        double degree = 3;
+        double cubes = Math.Pow(minN, degree);
+        Console.WriteLine($"{minN} |   {cubes}");  
+        minN++;
+    }  
+}
+
+Console.Write("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+findCubes(a);
+

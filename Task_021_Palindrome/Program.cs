86b36bcaf05089aff683641b81e3719ac3ac33b7
RenatBax <renchik_125.111@mail.ru>
2021-12-19 22:55:21 +0300

    Сделана задача 21

---
+++ Task_021_Palindrome/Program.cs
@@ -0,0 +1,12 @@
+﻿// 21. Программа проверяет пятизначное число на палиндромом.
+
+void palindrome(string text)
+{
+    if (text[0] == text[4] && text[1] == text[3]) Console.WriteLine("Введенное число являетcя палиндромом.");
+    else Console.WriteLine("Введенное число не палиндром.");
+}
+
+Console.Write("Введите пятизначное число: ");
+string S = Console.ReadLine();
+palindrome(S);
+

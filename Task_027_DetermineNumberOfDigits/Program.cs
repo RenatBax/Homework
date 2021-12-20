748f26e4f783b0fbe3ef352daf71adf5698a5416
RenatBax <renchik_125.111@mail.ru>
2021-12-20 22:20:36 +0300

    Сделана задача 27

---
+++ Task_027_DetermineNumberOfDigits/Program.cs
@@ -0,0 +1,13 @@
+﻿// 27. Определить количество цифр в числе
+
+double determineNumberOfDigits(string number)
+{
+    double length = number.Length;
+    double result = length;
+    return result;
+}
+
+Console.Write("Введите число: ");
+string N = Console.ReadLine();
+double digits = determineNumberOfDigits(N);
+Console.WriteLine($"Количество цифр в введенном числе: {digits}.");

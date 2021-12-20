80f86a69ee9f30d8ede35cb5dbf41e3d73c716ec
RenatBax <renchik_125.111@mail.ru>
2021-12-20 19:29:50 +0300

    Сделана задача 26

---
+++ Task_026_RaiseNumberToNaturalDegree/Program.cs
@@ -0,0 +1,20 @@
+﻿// 26. Возведите число А в натуральную степень B 
+//используя цикл
+
+double raiseNamberToDegree(double number, double numberDegree)
+{
+    double finiteNumber = 1;
+    for (double count = 0; count < numberDegree; count++)
+    {
+        finiteNumber = finiteNumber * number;
+    }
+    return finiteNumber;
+}
+
+Console.Write("Введите число А: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите натуральную степень В, в которую хотите возвести число А: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double result = raiseNamberToDegree(a, b);
+Console.WriteLine(result);

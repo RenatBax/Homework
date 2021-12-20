8bbfbd32774f423528834d3b1a9d5bd257a90337
RenatBax <renchik_125.111@mail.ru>
2021-12-20 18:51:10 +0300

    Сделана задача 25

---
+++ Task_025_SunNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿// 25. Найти сумму чисел от 1 до А
+
+void sumNambers(int a)
+{
+    int maxN = a;
+    int minN = 1;
+    //Console.WriteLine($"а | а х а"); 
+    while(minN <= maxN)
+    {
+        int sum = minN + minN;
+        Console.WriteLine(sum);  
+        minN++;
+    }  
+}
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+sumNambers(a);

ab82260b09d4c3f891cd6ccdeb5da023209bac87
RenatBax <renchik_125.111@mail.ru>
2021-12-21 01:37:07 +0300

    Сделана задача 30

---
+++ Task_030_ShowCubesEndingInEvenDigit/Program.cs
@@ -0,0 +1,28 @@
+﻿// 30. Показать кубы чисел, заканчивающихся на четную цифру
+
+void showCubes(double a)
+{
+    for (double minN = 1; minN <= a; minN++)
+    {
+        double showCubes = Math.Pow(minN, 3);
+        Console.WriteLine($" {minN} | {showCubes}");
+    }
+}
+
+void eventCubes(double a)
+{
+    Console.Write("Кубы, заканчивающие на четную цифру: ");
+    for (double minN = 1; minN <= a; minN++)
+    {
+        double cubes = Math.Pow(minN, 3);
+        if (cubes%2 == 0) Console.Write(cubes +" ");
+    }
+}
+
+Console.Write("Введите число A: ");
+double a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" A | A в кубе");
+showCubes(a);
+
+eventCubes(a);
+

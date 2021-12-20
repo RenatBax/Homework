8dd666ce97b71a43b8fd0f9c64766000481190de
RenatBax <renchik_125.111@mail.ru>
2021-12-20 23:52:56 +0300

    Сделана задача 29

---
+++ Task_029_ProductOfNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿// 29. Написать программу вычисления произведения чисел от 1 до N
+
+void ShowNumbers(int N)
+{
+    int maxN = N;
+    int minN = 1;
+    Console.Write("Интервал от введенного числа: ");
+    while (minN <= maxN)
+    {
+        Console.Write(minN +" ");  
+        minN++;
+    }  
+}
+
+double productNumbers(double a)
+{
+    double maxN = a;
+    double minN = 1;
+    double productNumbers = 1;
+    while (minN <= maxN)
+    {
+        double count = minN;
+        if (minN <= maxN)
+        {  
+            productNumbers = productNumbers * minN;
+            Console.WriteLine($"{minN} = {productNumbers}.");
+            count++;
+        }
+        minN++;
+    }
+    return productNumbers;
+}
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+ShowNumbers(N);
+Console.WriteLine();
+double result = productNumbers(N);
+Console.WriteLine($"Произведение чисел от 1 до N = {result}.");
+

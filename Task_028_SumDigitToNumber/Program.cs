313643d08759b4be881f446b20cbd592466d657b
RenatBax <renchik_125.111@mail.ru>
2021-12-22 12:50:49 +0300

    Сделана задача 31

---
+++ Task_028_SumDigitToNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿// 28. Подсчитать сумму цифр в числе
+
+int sumDigits(string number)
+{
+    //int length = number.Length;
+    int result = 0;
+    for (int i = 0; i < number.Length; i++)
+    {
+        //int j = number[0];
+        //if (j < length) 
+        result = result + number[i];
+        //j++;
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+string N = Console.ReadLine();
+int sum = sumDigits(N);
+Console.WriteLine($"Сумма цифр в введенном числе = {sum}.");
---
+++ Task_031_SetArrayOfEightElement/Program.cs
@@ -0,0 +1,12 @@
+﻿// 31. Задать массив из 8 элементов и вывести их на экран
+
+void setArray(int elements)
+{
+    int[] array = new int[elements];
+    for (int i = 0; i < elements; i++)
+    {
+        //array[i] = new Random().Next(0, 15);
+        Console.Write(array[i] +" ");
+    }
+}
+setArray(8);

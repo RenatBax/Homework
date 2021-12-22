8cf97ebf43b40a686392a618c0fe0b646391d51d
RenatBax <renchik_125.111@mail.ru>
2021-12-22 13:16:45 +0300

    Сделана задача 32

---
+++ Task_032_SetArrayFilledWithZerosAndOnes/Program.cs
@@ -0,0 +1,16 @@
+﻿// 32. Задать массив из 8 элементов, 
+// заполненных нулями и единицами 
+// вывести их на экран
+
+void setArrayZerosAndOnes(int elements)
+{
+    int[] zerosAndOnes = new int[elements];
+    for (int i = 0; i < elements; i++)
+    {
+        if (i%2 == 0) zerosAndOnes[i] = 0;
+        else zerosAndOnes[i] = 1;
+        Console.Write(zerosAndOnes[i] +" ");
+    }
+}
+
+setArrayZerosAndOnes(8);

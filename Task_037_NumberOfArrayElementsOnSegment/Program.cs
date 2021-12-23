5e0868a9ca1164ad608f0c82336723472af66b8e
RenatBax <renchik_125.111@mail.ru>
2021-12-24 02:39:56 +0300

    Сделана задача 37

---
+++ Task_037_NumberOfArrayElementsOnSegment/Program.cs
@@ -0,0 +1,45 @@
+﻿// 37. В одномерном массиве из 123 чисел 
+// найти количество элементов из отрезка [10,99]
+
+int[] createArray(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 124);
+    }
+    return array;
+}
+
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+}
+
+int[] elementsOfSegment(int[] array)
+{
+    int[] result = new int[99];
+    //for (int i = array.Length - 1; i >= 0; i--)
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i <= 8 || i >= 99)
+            continue;
+        result[i] = array[i];
+    }
+    return result;
+}
+
+int[] array123 = createArray(123);
+Console.Write($"Количество элементов 123: ");
+printArray(array123);
+Console.WriteLine();
+Console.WriteLine();
+int[] elements = elementsOfSegment(array123);
+
+Console.Write($"Количество элементов на отрезке [10,99]: ");
+for (int i = 0; i < elements.Length; i++)
+{
+    if (i >= 9) Console.Write(elements[i] + " ");
+}
+

342c4a55bd80b65f25b36f8ba2f0eb79691d658d
RenatBax <renchik_125.111@mail.ru>
2022-01-09 19:01:50 +0300

    сделана задача 47

---
+++ Task_047_CopyingAnArray/Program.cs
@@ -0,0 +1,19 @@
+﻿// 47. Написать программу копирования массива
+
+int[] arr = {1, 2, 3, 4, 5, 6};
+
+void printArray(int[] array, int copy)
+{
+    int t = 0;
+    while (t != copy)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] +" ");
+        }
+        Console.WriteLine();
+        t++;
+    }
+}
+
+printArray(arr, 7);

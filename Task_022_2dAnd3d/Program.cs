aa5ac9d8681ac21ecdfa9724eb78cd472b75f9b0
RenatBax <renchik_125.111@mail.ru>
2022-01-09 16:04:55 +0300

    Второй способ решения задачи 22

--- Task_022_2dAnd3d/Program.cs
+++ Task_022_2dAnd3d/Program.cs
@@ -12,3 +12,22 @@ void distanceSpace(double x1, double x2, double y1, double y2, double z1, double
 }
 
 distanceSpace(1, 5, 2, 6, 7, 9);
+
+//Второй способ
+
+double distance2D(double x1, double x2, double y1, 
+                double y2)
+{
+        return Math.Sqrt(Math.Pow(x2 - x1, 2) 
+                        + Math.Pow(y2 - y1, 2));
+}
+Console.WriteLine("Растояние в 2D " +distance2D(1, 5, 2, 6));
+
+double distance3D(double x1, double x2, double y1, 
+                double y2, double z1, double z2)
+{
+        return Math.Sqrt(Math.Pow(x2 - x1, 2) 
+                        + Math.Pow(y2 - y1, 2) 
+                        + Math.Pow(z2 - z1, 2));
+}
+Console.WriteLine("Растояние в 3D " +distance3D(1, 5, 2, 6, 7, 9));

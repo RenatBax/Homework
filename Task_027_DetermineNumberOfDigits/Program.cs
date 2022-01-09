abb202d1af6e796d03931190ac5f7d5088294449
RenatBax <renchik_125.111@mail.ru>
2022-01-09 15:05:50 +0300

    Второй способ решения задачи 27

--- Task_027_DetermineNumberOfDigits/Program.cs
+++ Task_027_DetermineNumberOfDigits/Program.cs
@@ -11,3 +11,17 @@ Console.Write("Введите число: ");
 string N = Console.ReadLine();
 double digits = determineNumberOfDigits(N);
 Console.WriteLine($"Количество цифр в введенном числе: {digits}.");
+
+// Второй способ, но неработает если знаков больше 9????
+int calculateDigits(int number)
+{
+    int calculate = 0;
+    while (number != 0)
+    {
+        calculate++;
+        number = number/10;
+    }
+    return calculate;
+}
+int digit = Convert.ToInt32(N);
+Console.WriteLine("Второй способ: " +calculateDigits(digit));
--- Task_039_ProductOfPairsOfNumbersInAnArray/Program.cs
+++ Task_039_ProductOfPairsOfNumbersInAnArray/Program.cs
@@ -15,60 +15,93 @@ void printArray(int[] array)
     Console.Write(array[i] +" ");
 }
 
-// int[] firstPartOfArray(int[] array)
-// {
-//     int[] result = new int[6];
-//     int a = array.Length;
-//     int b = array.Length / 2;
-//     int c = a - b;
-//     for(int i = 0; i < b; i++)
-//     {
-//         result[i] = array[i];
-//     }
-//     return result;
-// }
+int[] firstPartOfArray(int[] array)
+{
+    int a = array.Length;
+    int b = array.Length / 2;
+    int c = a - b;
+    int[] result = new int[b];
+    for (int i = 0; i < b; i++)
+    {
+        result[i] = array[i];
+    }
+    return result;
+}
 
-// int[] secondPartOfArray(int[] array)
-// {
-//     int[] result = new int[12];
-//     int a = array.Length;
-//     int b = array.Length / 2;
-//     int c = a - b;
-//     for(int i = a - 1; i >= c; i--)
-//     {
-//         result[i] = array[i];
-//     }
-//     return result;
-// }
+int[] secondPartOfArray(int[] array)
+{
+    int a = array.Length;
+    int b = array.Length / 2;
+    int c = a - b; 
+    int j = 0;  
+    for (int i = a - 1; i >= c; i--)
+    { 
+        array[j] = array[i];
+        j++;
+    }
+    int[] result = new int[j];
+    int m = 0;
+    for (int i = 0; i < result.Length; i++)
+    { 
+        result[m] = result[i];
+        m++;
+    }
+    return result;
+}
 
 int[] mainArray = fullArray(12);
 Console.Write("Одномерный массив из 12 элементов: ");
 printArray(mainArray);
 
 Console.WriteLine();
-// int[] fP = firstPartOfArray(mainArray);
-// printArray(fP);
+int[] fP = firstPartOfArray(mainArray);
+printArray(fP);
+
+Console.WriteLine();
+int[] sP = secondPartOfArray(mainArray);
+printArray(sP);
+
+
+// int[] zeroOffArray(int[] array)
+// {
+//     int temp = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] != 0)
+//             array[temp++] = array[i];
+//     }
+//     int[] zeNumber = new int[temp];
+//     Array.Copy(array, 0, zeNumber, 0, temp);
+//     return zeNumber;
+// }
+
+// int[] zeroOff = zeroOffArray(eventNumbers);
+// printArray(zeroOff);
+
+
+
 
 // Console.WriteLine();
-// int[] sP = secondPartOfArray(mainArray);
-// printArray(sP);
-// int productNumber = productOfPairsOfArray(first);
-// Console.WriteLine($"Произведение чисел, стоящих на крайних позициях: {productNumber}.");
+// // int[] sP = secondPartOfArray(mainArray);
+// // printArray(sP);
+// // int productNumber = productOfPairsOfArray(first);
+// // Console.WriteLine($"Произведение чисел, стоящих на крайних позициях: {productNumber}.");
 
-int a = mainArray.Length;
-int b = mainArray.Length / 2;
-int c = a - b;
+// int a = mainArray.Length;
+// int b = mainArray.Length / 2;
+// int c = a - b;
 
-for(int i = 0; i < b; i++)
-{
-    Console.Write(mainArray[i] + " ");
-}
-Console.WriteLine();
-for (int j = a - 1; j >= c; j--) //(int j = c; j < a; j++)
-{
-    Console.Write(mainArray[j] +" ");
-}
+// // for(int i = 0; i < b; i++)
+// // {
+// //     Console.Write(mainArray[i] + " ");
+// // }
+// //Console.WriteLine();
+// int[] arr = new int[a];
+// for (int j = a - 1; j >= c; j--) //(int j = c; j < a; j++)
+
     
+//     arr[j] = mainArray[j];
+//     //Console.Write(arr[j] +" ");
 
 
-
+// printArray(arr);

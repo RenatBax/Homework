98481f098216cc45d330a033514762463a1b5454
RenatBax <renchik_125.111@mail.ru>
2022-01-13 19:13:30 +0300

    Сделана задача 49

--- Task_039_ProductOfPairsOfNumbersInAnArray/Program.cs
+++ Task_039_ProductOfPairsOfNumbersInAnArray/Program.cs
@@ -15,36 +15,18 @@ void printArray(int[] array)
     Console.Write(array[i] +" ");
 }
 
-int[] firstPartOfArray(int[] array)
+int[] productOfPartOfArray(int[] array)
 {
     int a = array.Length;
     int b = array.Length / 2;
-    int c = a - b;
     int[] result = new int[b];
-    for (int i = 0; i < b; i++)
+    int i = 0;
+    int j = a - 1;
+    while (i < b)
     {
-        result[i] = array[i];
-    }
-    return result;
-}
-
-int[] secondPartOfArray(int[] array)
-{
-    int a = array.Length;
-    int b = array.Length / 2;
-    int c = a - b; 
-    int j = 0;  
-    for (int i = a - 1; i >= c; i--)
-    { 
-        array[j] = array[i];
-        j++;
-    }
-    int[] result = new int[j];
-    int m = 0;
-    for (int i = 0; i < result.Length; i++)
-    { 
-        result[m] = result[i];
-        m++;
+        result[i] = array[i] * array[j];
+        i++;
+        j--;
     }
     return result;
 }
@@ -53,55 +35,6 @@ int[] mainArray = fullArray(12);
 Console.Write("Одномерный массив из 12 элементов: ");
 printArray(mainArray);
 
-Console.WriteLine();
-int[] fP = firstPartOfArray(mainArray);
-printArray(fP);
-
-Console.WriteLine();
-int[] sP = secondPartOfArray(mainArray);
-printArray(sP);
-
-
-// int[] zeroOffArray(int[] array)
-// {
-//     int temp = 0;
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         if (array[i] != 0)
-//             array[temp++] = array[i];
-//     }
-//     int[] zeNumber = new int[temp];
-//     Array.Copy(array, 0, zeNumber, 0, temp);
-//     return zeNumber;
-// }
-
-// int[] zeroOff = zeroOffArray(eventNumbers);
-// printArray(zeroOff);
-
-
-
-
-// Console.WriteLine();
-// // int[] sP = secondPartOfArray(mainArray);
-// // printArray(sP);
-// // int productNumber = productOfPairsOfArray(first);
-// // Console.WriteLine($"Произведение чисел, стоящих на крайних позициях: {productNumber}.");
-
-// int a = mainArray.Length;
-// int b = mainArray.Length / 2;
-// int c = a - b;
-
-// // for(int i = 0; i < b; i++)
-// // {
-// //     Console.Write(mainArray[i] + " ");
-// // }
-// //Console.WriteLine();
-// int[] arr = new int[a];
-// for (int j = a - 1; j >= c; j--) //(int j = c; j < a; j++)
-
-    
-//     arr[j] = mainArray[j];
-//     //Console.Write(arr[j] +" ");
-
-
-// printArray(arr);
+Console.Write("\nПроизведение пар чисел: ");
+int[] productOfPart = productOfPartOfArray(mainArray);
+printArray(productOfPart);

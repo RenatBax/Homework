d96aa6834cf94e3732814f9cada41340ed8ff5cf
RenatBax <renchik_125.111@mail.ru>
2022-01-15 19:49:07 +0300

    Сделана задача 40

---
+++ Task_040_DifferenceBetweenMaxMinElements/Program.cs
@@ -0,0 +1,77 @@
+﻿// 40. В Указанном массиве вещественных чисел 
+// найдите разницу между максимальным и минимальным элементом
+
+double[] arrayOfRealNumbers(int n)
+{
+    double[] array = new double[n];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.NextDouble();
+    }
+    return array;
+}
+
+void showArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.WriteLine($" {i} - {array[i]}");
+}
+
+string differenceBetweenPosition(double[] array)
+{
+    string differencePosition = string.Empty;
+    int difference = 0;
+    int minPosition = 0;
+    int maxPosition = 0;
+    int size = array.Length;
+    int j = 0;
+    while (j < size)
+    {
+        if (array[j] < array[minPosition])
+        {
+            minPosition = j;
+        }
+        if (array[j] > array[minPosition] && array[j] > array[maxPosition])
+        {
+            maxPosition = j;
+        }
+        j++;
+        difference = minPosition - maxPosition;
+    }
+    return $"Разница в позиции элемента: {minPosition} - {maxPosition} = {difference}";
+}
+
+void differenceBetweenNumber(double[] array)
+{
+    double difference = 0;
+    int minPosition = 0;
+    double min = array[minPosition];
+    double max = 0;
+    int maxPosition = 0;
+    int size = array.Length;
+    int j = 0;
+    while (j < size)
+    {
+        if (array[j] < array[minPosition])
+        {
+            min = array[j];
+            minPosition = j;
+        }
+        if (array[j] > array[minPosition] && array[j] > array[maxPosition])
+        {
+            max = array[j];
+            maxPosition = j;
+        }
+        j++;
+        difference =  max - min;
+    }
+    Console.WriteLine($"Разница в позиции числа: {max} - {min} = {difference}");
+}
+
+double[] arrayOfReal = arrayOfRealNumbers(10);
+Console.WriteLine("Массив вещественных чисел: ");
+showArray(arrayOfReal);
+string difference = differenceBetweenPosition(arrayOfReal);
+Console.WriteLine(difference);
+differenceBetweenNumber(arrayOfReal);
---
+++ Task_043_ConversionToBinary/Program.cs
@@ -0,0 +1,19 @@
+﻿// 43. Написать программу преобразования десятичного числа в двоичное
+
+int conversionBinary(int decim)
+{
+    int binary = 0;
+    int count = decim%2;
+    do 
+    {
+        binary = count/2;
+        binary++;
+    } while (binary < 0);
+
+    return binary;
+}
+
+Console.Write("Введите десятичное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int b = conversionBinary(number);
+Console.WriteLine(b);

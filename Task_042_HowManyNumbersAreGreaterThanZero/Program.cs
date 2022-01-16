bf160d2c1e354682193938593fd6d891b7d8490c
RenatBax <renchik_125.111@mail.ru>
2022-01-16 22:03:42 +0300

    Сделана задача 42

---
+++ Task_042_HowManyNumbersAreGreaterThanZero/Program.cs
@@ -0,0 +1,24 @@
+﻿// 42. Определить сколько чисел больше 0 введено с клавиатуры
+
+void numbersAreGreaterZero(string number)
+{
+    int[] array = new int[5];
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write("Введите число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    int count = 0;
+    int j = 0;
+    int size = array.Length;
+    while (j < size)
+    {
+        if (array[j] > 0) { count++; }
+        j++;
+    }
+    Console.Write("Количество введенных чисел больше 0 = " + count);
+}
+
+Console.WriteLine("Введите 5 различных целых чисел!");
+Console.WriteLine("Нажмите клавишу ENTER");
+numbersAreGreaterZero(Console.ReadLine());
